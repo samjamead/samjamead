20e9abbc1d0e39b5594f7ddbb7cea6f20fb65b01
Sam Mead <samjamead@gmail.com>
2023-11-27 19:26:31 +0000

    Attempted LinkedIn embed

---
+++ app/linkedin/page.tsx
@@ -0,0 +1,15 @@
+export default function LinkedIn() {
+  return (
+    <div>
+      <h1 className='mb-8'>Testing LinkedIn Embeds</h1>
+      <hr className='mb-8' />
+
+      <iframe
+        src='https://www.linkedin.com/embed/feed/update/urn:li:share:7134796899659702272'
+        height='688'
+        width='504'
+        title='Embedded post'
+      ></iframe>
+    </div>
+  );
+}
