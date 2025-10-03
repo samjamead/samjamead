fbc2107f11e2a4e3cf04554a210a2a20a9495a73
Sam Mead <samjamead@gmail.com>
2025-10-03 16:41:55 +0100

    A bit of spacing

--- app/page.tsx
+++ app/page.tsx
@@ -35,12 +35,19 @@ export default function Home() {
           Tanzan and Ekido were once traveling together down a muddy road. A
           heavy rain was still falling. Coming around a bend, they met a lovely
           girl in a silk kimono and sash, unable to cross the intersection.
-          "Come on, girl," said Tanzan at once. Lifting her in his arms, he
-          carried her over the mud. Ekido did not speak again until that night
-          when they reached a lodging temple. Then he could no longer restrain
-          himself. "We monks don't go near females," he told Tanzan, "especially
-          not young and lovely ones. It is dangerous. Why did you do that?" "I
-          left the girl there," said Tanzan. "Are you still carrying her?”
+        </p>
+        <p>"Come on, girl" said Tanzan at once.</p>
+        <p>
+          Lifting her in his arms, he carried her over the mud. Ekido did not
+          speak again until that night when they reached a lodging temple. Then
+          he could no longer restrain himself.
+        </p>
+        <p>
+          "We monks don't go near females," he told Tanzan, "especially not
+          young and lovely ones. It is dangerous. Why did you do that?"
+        </p>
+        <p>
+          "I left the girl there" said Tanzan. "Are you still carrying her?”
         </p>
 
         <Heading2>
